ff3199e0a6e3c5f7002163f413e95fcedb50284b
Jambr0li <jasonkovalenko@gmail.com>
2024-10-15 08:20:02 -0700

    did car fleet (medium) w/o help

--- car_fleet/main.py
+++ car_fleet/main.py
@@ -1,12 +1,17 @@
 def carFleet(target, position, speed):
-	pairs = [(p,s) for p, s in zip(position,speed)]
+	pairs = [[x,y] for x,y in zip(position,speed)]
+	pairs.sort(reverse=True)
+	print(pairs)
 
 	stack = []
-	for p, s in sorted(pairs)[::-1]:
+	for p, s in pairs:
 		stack.append((target - p) / s)
-		if len(stack) >= 2 and stack [-1] <= stack[-2]:
+		print(stack)
+		if len(stack) >= 2 and stack[-1] <= stack[-2]:
 			stack.pop()
-	return len(stack) 
+	return len(stack)
+
+
 
 print(carFleet(10,[1,4],[3,2]))
 print(carFleet(10,[4,1,0,7],[2,2,1,1]))
