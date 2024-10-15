67eaf0c0f7aaea17f537c7c667a01f9f2fa61901
Jambr0li <jasonkovalenko@gmail.com>
2024-10-14 18:59:42 -0700

    did daily temps w/o looking this time

--- daily_temperatures/main.py
+++ daily_temperatures/main.py
@@ -1,12 +1,13 @@
 def dailyTemperatures(temperatures):
 	stack = []
-	res = [0] * len(temperatures)
-	for i, t in enumerate(temperatures):
-		while stack and t > stack[-1][0]: # while stack has something in it and the current temperature is greater than the top of the stack (highest temp)
-			stackT, stackInd = stack.pop()
-			res[stackInd] = i - stackInd 
-		stack.append([t,i])
-	return res
+	result = [0] * len(temperatures)
+	for i,t in enumerate(temperatures):
+		while stack and stack[-1][1] < t:
+			stackInd, stackTemp = stack.pop()
+			result[stackInd] = i - stackInd
+		stack.append([i,t])
+	return result
 
-print(dailyTemperatures([22,21,20]))
+
+
 print(dailyTemperatures([30,38,30,36,35,40,28]))
