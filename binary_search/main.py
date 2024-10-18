5c6c3e79ad931cb6599c5f90a432e2d156831fd0
Jambr0li <jasonkovalenko@gmail.com>
2024-10-18 12:56:32 -0700

    binary search (easy), harder than it should've been...

---
+++ binary_search/main.py
@@ -0,0 +1,17 @@
+def search(nums, target):
+	l, r = 0, len(nums) - 1
+
+	while l <= r:
+		c = l + (r - l // 2)
+		if nums[c] < target:
+			l = c + 1
+		elif nums[c] > target:
+			r = c - 1
+		else:
+			return c
+	return -1
+
+
+
+print(search([-1,0,2,4,6,8], 4))
+print(search([-1,0,2,4,6,8], 3))
