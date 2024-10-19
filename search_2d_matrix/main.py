5fd7e26c6ad636ee9972a0f373265c7c00119bcd
Jambr0li <jasonkovalenko@gmail.com>
2024-10-19 09:35:59 -0700

    did 2d binary search again, I should do it a 3rd time later...

--- search_2d_matrix/main.py
+++ search_2d_matrix/main.py
@@ -1,33 +1,35 @@
 def searchMatrix(matrix, target):
 	ROWS, COLS = len(matrix), len(matrix[0])
 
-	top, bot = 0, ROWS - 1
-	while top <= bot:
-		row = (top + bot) // 2
+	t, b = 0, ROWS - 1
+	while t <= b:
+		row = t + (b - t) // 2
 		if target > matrix[row][-1]:
-			top = row + 1
+			t = row + 1
 		elif target < matrix[row][0]:
-			bot = row - 1
+			b = row - 1
 		else:
 			break
 
-	if not (top <= bot):
+	if not (t <= b):
 		return False
 
-	row = (top + bot) // 2
 	l, r = 0, COLS - 1
 	while l <= r:
-		m = (l + r) // 2
-		if target > matrix[row][m]:
-			l = m + 1
-		elif target < matrix[row][m]:
-			r = m - 1
-		else:cd
+		c = l + (r - l) // 2
+		if matrix[row][c] > target:
+			r = c - 1
+		elif matrix[row][c] < target:
+			l = c + 1
+		else:
 			return True
+		print("bot")
 	return False
+
+
 
 
 # print(searchMatrix([[1,2,4,8],[10,11,12,13],[14,20,30,40]], 10))
 # print(searchMatrix([[1,2,4,8],[10,11,12,13],[14,20,30,40]], 15))
-# print(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]],3))
-print(searchMatrix([[1],[3]], 3))
+print(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]],3))
+# print(searchMatrix([[1],[3]], 3))
