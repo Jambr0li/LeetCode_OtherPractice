2bbd7c6f8e5d414758a99b60fd6ba090b410ca68
Jambr0li <jasonkovalenko@gmail.com>
2024-10-23 11:04:33 -0700

    did it a second time

--- time_based_key_value_store/main.py
+++ time_based_key_value_store/main.py
@@ -1,28 +1,74 @@
+# class TimeMap:
+#     def __init__(self):
+#         self.keyStore = {}
+         
+
+#     def set(self, key: str, value: str, timestamp: int) -> None:
+#         if key not in self.keyStore:
+#             self.keyStore[key] = []
+#         self.keyStore[key].append([value,timestamp])
+        
+
+#     def get(self, key: str, timestamp: int) -> str:
+#         res, values = "", self.keyStore.get(key, [])
+#         l, r = 0, len(values) - 1
+#         while l <= r:
+#             m = (l + r) // 2
+#             if values[m][1] <= timestamp:
+#                 res = values[m][0]
+#                 l = m + 1
+#             else:
+#                 r = m - 1
+#         return res
+        
+
+
+
+
+
+
+
+
+
+
+
+
 class TimeMap:
     def __init__(self):
         self.keyStore = {}
-         
 
     def set(self, key: str, value: str, timestamp: int) -> None:
         if key not in self.keyStore:
             self.keyStore[key] = []
-        self.keyStore[key].append([value,timestamp])
-        
+        self.keyStore[key].append([timestamp,value])
 
     def get(self, key: str, timestamp: int) -> str:
-        res, values = "", self.keyStore.get(key, [])
-        l, r = 0, len(values) - 1
+        # binary search and keep minimum
+        values = self.keyStore.get(key,[])
+        res = ""
+        l, r = 0,len(values) - 1
         while l <= r:
-            m = (l + r) // 2
-            if values[m][1] <= timestamp:
-                res = values[m][0]
-                l = m + 1
+            c = l + (r - l) // 2
+            if values[c][0] <= timestamp:
+                res = values[c][1]
+                l = c + 1
             else:
-                r = m - 1
+                r = c - 1
         return res
-        
+
+
+
 test =  TimeMap()
 
 test.set("alice","happy",1)
 test.set("alice","sad",10)
-print(test.get("alice", 9))
+print(test.get("alice", 111))
+
+
+
+
+
+
+
+
+
