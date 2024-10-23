8d2246ca3375576d3b6acdb37827dccf5883c090
Jambr0li <jasonkovalenko@gmail.com>
2024-10-22 18:36:04 -0700

    did it again after watching video:

--- longest_substring_without_duplicates/main.py
+++ longest_substring_without_duplicates/main.py
@@ -1,18 +1,34 @@
+# My original solution
+# def lengthOfLongestSubstring(s):
+# 	# if len(s) == 1:
+# 		# return 1
+# 	s = list(s)
+# 	start = -1
+# 	maxL = 0
+# 	letter_map = {}
+# 	for i,l in enumerate(s):
+# 		if l in letter_map:
+# 			start = max(start,letter_map[l])
+# 		maxL = max(maxL,i - start)
+# 		letter_map[l] = i
+# 		print(letter_map)
+# 		print(maxL)
+# 	return maxL
+
+# After watching neetcode
 def lengthOfLongestSubstring(s):
-	# if len(s) == 1:
-		# return 1
-	s = list(s)
-	start = -1
+	l = 0
+	lSet = set()
 	maxL = 0
-	letter_map = {}
-	for i,l in enumerate(s):
-		if l in letter_map:
-			start = max(start,letter_map[l])
-		maxL = max(maxL,i - start)
-		letter_map[l] = i
-		print(letter_map)
-		print(maxL)
+	for r in range(len(s)):
+		while s[r] in lSet:
+			lSet.remove(s[l])
+			l += 1
+		lSet.add(s[r])
+		maxL = max(maxL, r - l + 1)
 	return maxL
+
+
 
 
 
