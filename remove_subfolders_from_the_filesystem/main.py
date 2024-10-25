fc11525f838c95bdbe5a435b2c9a85b5b7de1f10
Jambr0li <jasonkovalenko@gmail.com>
2024-10-25 15:27:48 -0700

    did it again much faster and cleaner version

--- remove_subfolders_from_the_filesystem/main.py
+++ remove_subfolders_from_the_filesystem/main.py
@@ -1,18 +1,30 @@
-def removeSubfolders(folders):
-	folders.sort()
-	fs = []
-	print("cc".startswith("c"))
-	for folder in folders:
-		has_it = False
-		for f in fs:
-			split_f = f.split("/")
-			split_folder = folder.split("/")
-			print(split_f)
-			print(split_folder)
-			if split_f == split_folder[0:len(split_f)]:
-				has_it = True 
-		if not has_it:
-			fs.append(folder)
-	return fs
+# def removeSubfolders(folders):
+# 	folders.sort()
+# 	fs = []
+# 	print("cc".startswith("c"))
+# 	for folder in folders:
+# 		has_it = False
+# 		for f in fs:
+# 			split_f = f.split("/")
+# 			split_folder = folder.split("/")
+# 			print(split_f)
+# 			print(split_folder)
+# 			if split_f == split_folder[0:len(split_f)]:
+# 				has_it = True 
+# 		if not has_it:
+# 			fs.append(folder)
+# 	return fs
+
+def removeSubfolders(folder):
+        folder.sort()
+        fs = [folder[0]]
+
+        for i in range(1, len(folder)):
+            prev_folder = fs[-1] + "/"
+
+            if not folder[i].startswith(prev_folder):
+                fs.append(folder[i])
+        return fs
+
 # print(removeSubfolders(["/a","/a/b","/c/d","/c/d/e","/c/f"]))
 print(removeSubfolders(["/ah/al/am","/ah/al"]))
